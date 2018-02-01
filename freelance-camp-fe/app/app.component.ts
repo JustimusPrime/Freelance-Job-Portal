ff046b4470bfc30fc75b6f0bd0131f723b4eb058
JustimusPrime <hirejustin@yahoo.com>
2018-02-01 14:31:58 -0700

    refactored bootstrap to AppComponent to begin app organization

---
+++ freelance-camp-fe/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app',
+  template: '<h1>Navigation</h1>'
+})
+export class AppComponent {
+  title: 'Freelance Bootcamp Dashboard';
+}
+
+
+// this is like the application.html.erb, sort of the template for the site, more flexibility
