2f105636903e3511b63dd477b92434c91c23c2d1
JustimusPrime <hirejustin@yahoo.com>
2018-02-05 10:46:30 -0700

    set up id params for show page and connected to proposal index

--- freelance-camp-fe/app/proposal/proposal-show.component.js
+++ freelance-camp-fe/app/proposal/proposal-show.component.js
@@ -5,19 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router");
 var ProposalShowComponent = /** @class */ (function () {
-    function ProposalShowComponent() {
+    function ProposalShowComponent(route) {
+        this.route = route;
     }
+    ProposalShowComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.routeId = this.route.params.subscribe(function (params) {
+            _this.id = +params['id'];
+        });
+    };
     ProposalShowComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'proposal-show',
             templateUrl: 'proposal-show.component.html'
-        })
+        }),
+        __metadata("design:paramtypes", [router_1.ActivatedRoute])
     ], ProposalShowComponent);
     return ProposalShowComponent;
 }());
 exports.ProposalShowComponent = ProposalShowComponent;
+// dependency injection - private - value of argument - data type
+// want to let component know that as soon as new class has been istantiated (when someone goes to the proposal show page) it will immediately call this constructor, which will call an instance of this activated route and store it in this route argument which we can use throughout the rest of our class
+// subcribe - something that gives access to the object you wanna communicate with. 
 //# sourceMappingURL=proposal-show.component.js.map
--- freelance-camp-fe/app/proposal/proposal-show.component.ts
+++ freelance-camp-fe/app/proposal/proposal-show.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-show',
   templateUrl: 'proposal-show.component.html'
 })
-export class ProposalShowComponent {}
+export class ProposalShowComponent implements OnInit {
+  id: number;
+  routeId: any;
+
+  constructor(
+    private route: ActivatedRoute
+    ) {}
+
+  ngOnInit(): void {
+    this.routeId = this.route.params.subscribe(
+      params => {
+         this.id = +params['id'];
+      }
+    )
+  }
+}
+
+// dependency injection - private - value of argument - data type
+// want to let component know that as soon as new class has been istantiated (when someone goes to the proposal show page) it will immediately call this constructor, which will call an instance of this activated route and store it in this route argument which we can use throughout the rest of our class
+
+// subcribe - something that gives access to the object you wanna communicate with. 
