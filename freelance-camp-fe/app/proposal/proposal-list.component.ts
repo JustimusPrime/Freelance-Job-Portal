8dc4d97a996e0d387a377726bf06e6e9a6c70072
JustimusPrime <hirejustin@yahoo.com>
2018-02-05 22:11:58 -0700

    integrted list-group on proposal list

--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -5,6 +5,7 @@ import { Proposal } from './proposal';
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html'
+  styleUrls: ['proposal-list.component.css'],
 })
 export class ProposalListComponent {
   proposalOne: Proposal = new Proposal(15, 'abc company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 150, 120, 15, 'jordan@de=vcamp.com')
