e762104ebdd4099b952bd79d798b2c136780e9de
JustimusPrime <hirejustin@yahoo.com>
2018-02-06 16:01:29 -0700

    used observable to stream data for proposal list

--- freelance-camp-fe/app/proposal/proposal-list.component.ts
+++ freelance-camp-fe/app/proposal/proposal-list.component.ts
@@ -1,20 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 import { Proposal } from './proposal';
+import { ProposalService } from './proposal.service';
 
 @Component({
   moduleId: module.id,
   selector: 'proposal-list',
   templateUrl: 'proposal-list.component.html',
   styleUrls: ['proposal-list.component.css'],
+  providers: [ ProposalService ]
 })
-export class ProposalListComponent {
-  proposalOne: Proposal = new Proposal(15, 'abc company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 20, 125, 15, 'jordan@de=vcamp.com')
-  proposalTwo: Proposal = new Proposal(99, 'xyz company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 20, 125, 15, 'jordan@de=vcamp.com')
-  proposalThree: Proposal = new Proposal(300, 'something company', 'http://portfolio.jordanhudgens.com', 'Ruby on Rails', 20, 125, 15, 'jordan@de=vcamp.com')    
-  proposals: Proposal[] = [
-    this.proposalOne, 
-    this.proposalTwo, 
-    this.proposalThree
-    ]
+export class ProposalListComponent implements OnInit {  
+  proposals: Proposal[];
+  errorMessage: string;
+  mode = "Observable";
+
+  constructor(
+    private proposalService: ProposalService
+  ) {}
+
+  ngOnInit() {
+    let timer = Observable.timer(0, 5000);
+    timer.subscribe(() => this.getProposals());
+  }
+
+  getProposals() {
+    this.proposalService.getProposals()
+        .subscribe(
+          proposals => this.proposals = proposals,
+          error => this.errorMessage = <any>error
+        );
+  }  
 }
 
